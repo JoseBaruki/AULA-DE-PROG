8b796e89752c335a59def59d32d26e2dd59eea22
JoseBaruki <112413623+JoseBaruki@users.noreply.github.com>
2022-09-12 11:46:10 -0400

    Add files via upload

---
+++ aula-12-09-Tabuada.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+
+using namespace std;
+
+int main()
+
+
+{
+     cout<<"Informe a tabuada que deseja calcular : \n";
+
+    int n = 0;
+    cin>>n;
+
+    for (int i=1; i<=10; i++){
+    cout << i << " x " << n << " = " << i * n<< "\n";
+
+
+    }
+
+return 0;
+
+
+}
